0ce2248c0b85e02761363592be35feeb65813a46
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-17 14:55:07 -0800

    compile time run time

--- Introduction to C++ for DSA/1-1 Week 01 Conceptual Session 01/pointer.cpp
+++ Introduction to C++ for DSA/1-1 Week 01 Conceptual Session 01/pointer.cpp
@@ -2,11 +2,29 @@
 using namespace std;
 int main()
 {
-    int a = 10; // stack er moddhe save hocche
-    int *p;     // address variable k amra pointer name chini
-    p = &a;     // a er address hold kortese , reference kora
-    *p = 100;   // dereference
-    cout << a << endl;
+    // int a = 10; // stack er moddhe save hocche
+    // int *p;     // address variable k amra pointer name chini
+    // p = &a;     // a er address hold kortese , reference kora
+    // *p = 100;   // dereference
+    // cout << a << endl;
 
+    // dynamic array static
+    // int a[5] = {1, 2, 3, 4, 5};
+    // int *p;
+    // // p = a; // a er first value er address ta thakbe
+    // p = &a[0];
+    // cout << p << end1;
+    // return 0;
+
+    // dynamic start
+    // int *arr = new int[10];
+    // arr[0] = 10;
+    // arr[1] = 200;
+    // delete[] arr;
+
+    // compile time
+    int a = 10, b = 0;
+    cout << a / b << endl;
+    cout << "Print Successfull";
     return 0;
 }
