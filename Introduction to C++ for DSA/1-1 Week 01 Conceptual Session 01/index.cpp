951e04c3aae6f0a72ade984e39f9c19cfead12b3
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-17 12:38:26 -0800

    c++ for DSA

---
+++ Introduction to C++ for DSA/1-1 Week 01 Conceptual Session 01/index.cpp
@@ -0,0 +1,14 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int a = 10;
+//     return 0;
+// }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 10;
+    return 0;
+}
