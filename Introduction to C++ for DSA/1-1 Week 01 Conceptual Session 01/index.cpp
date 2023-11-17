a886db94cac669d72e46bfc0137927927e4a65fb
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-17 12:58:32 -0800

    stack

--- Introduction to C++ for DSA/1-1 Week 01 Conceptual Session 01/index.cpp
+++ Introduction to C++ for DSA/1-1 Week 01 Conceptual Session 01/index.cpp
@@ -7,8 +7,23 @@
 // }
 #include <bits/stdc++.h>
 using namespace std;
+
+void fun2()
+{
+    int n = 30;
+    cout << "I am from fun2" << endl;
+}
+
+void fun1()
+{
+    int n = 20;
+    fun2();
+    cout << "I am from fun1" << endl;
+}
 int main()
 {
     int a = 10;
+    cout << "I am from fun main" << endl;
+    fun1();
     return 0;
 }
