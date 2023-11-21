65052ccf50e62c6123730980e32278326f37a9e7
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-20 21:11:19 -0800

    ternary operator

--- Introduction to C++ for DSA/Module 1 - Basic C++/setup/condition_1.cpp
+++ Introduction to C++ for DSA/Module 1 - Basic C++/setup/condition_1.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
-    int x = 10;
-    if (x == 10)
-    {
-        cout << "It is ten out of ten\n";
-    }
-    else
-    {
-        cout << "Not ten\n";
-    }
-    return 0;
+    // int x = 10;
+    // if (x == 10)
+    // {
+    //     cout << "It is ten out of ten\n";
+    // }
+    // else
+    // {
+    //     cout << "Not ten\n";
+    // }
+
+        return 0;
 }
