c609b27a0ce414c091e561c1342b29c31135d10b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-21 13:37:42 -0800

    dynamic or heap in c++

---
+++ Introduction to C++ for DSA/Module 1 - Basic C++/setup/bits_header_file.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+
+// #include <iostream>
+// #include <string>
+// #include <iomanip>   // input and output manipulation sepration
+// #include <algorithm> //min and max
+// #include <utility>   //swap
+// #include <string.h>  // getline
+// #include <stdio.h>   //
+using namespace std;
+
+int main()
+{
+    cout << " hello" << endl;
+}
