f4d81eeba815a385103f34f0859f67df24819599
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-20 22:40:55 -0800

    done switch

---
+++ Introduction to C++ for DSA/Module 1 - Basic C++/setup/switch_case.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 33;
+    switch (x)
+    {
+    case 1:
+        cout << "One" << endl;
+        break;
+    case 2:
+        cout << "Two" << endl;
+        break;
+    case 3:
+        cout << "Three" << endl;
+        break;
+    case 4:
+        cout << "Four" << endl;
+        break;
+    case 5:
+        cout << "Five" << endl;
+        break;
+    default:
+        cout << "Nothing" << endl;
+    }
+
+    return 0;
+}
