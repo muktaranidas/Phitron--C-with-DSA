214e76370cd4ef116fe921d886fc0272c7126398
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-20 20:47:01 -0800

    done c++ input

---
+++ Introduction to C++ for DSA/Module 1 - Basic C++/setup/input.cpp
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+
+    cin >> a >> b >> c;
+    // int ascii = c;
+    cout << a << " " << b << " " << int(c) << endl;
+
+    return 0;
+}
