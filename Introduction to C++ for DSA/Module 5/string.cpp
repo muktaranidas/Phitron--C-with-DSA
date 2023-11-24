be38c5cfbc851b6d5c1099dc5397ccc9866a7e36
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-24 12:57:59 -0800

    done string

--- Introduction to C++ for DSA/Module 5/string.cpp
+++ Introduction to C++ for DSA/Module 5/string.cpp
@@ -5,8 +5,29 @@ int main()
     // char a[10] = "Hello";
     // cout << a << endl;
     // cout << strlen(a) << endl;
-    string s = "Hello";
-    s = "gelo hi";
-    cout << s << endl;
+    // char s1 = "Mukta";
+    // char s2 = "Thakur";
+    // if (strcmp(s1, s2) == 0)
+    // {
+    //     cout << "same" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not same" << endl;
+    // }
+    // string start
+    //  string s = "Hello";
+    //  s = "gelo hi";
+    //  cout << s << endl;
+    string s1 = "Mukta";
+    string s2 = "Thakur";
+    if (s1 == s2)
+    {
+        cout << "same" << endl;
+    }
+    else
+    {
+        cout << "Not same" << endl;
+    }
     return 0;
 }
