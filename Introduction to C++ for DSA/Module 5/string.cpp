f0b8c9790d426e9ce14edc14a697102ec2955f80
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-24 12:50:43 -0800

    string

--- Introduction to C++ for DSA/Module 5/string.cpp
+++ Introduction to C++ for DSA/Module 5/string.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main()
 {
-    char a[10] = "Hello";
-    cout << a << endl;
-    cout << strlen(a) << endl;
+    // char a[10] = "Hello";
+    // cout << a << endl;
+    // cout << strlen(a) << endl;
+    string s = "Hello";
+    s = "gelo hi";
+    cout << s << endl;
     return 0;
 }
