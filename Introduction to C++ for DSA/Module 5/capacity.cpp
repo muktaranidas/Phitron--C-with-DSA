22e67b23f7aa9636c0421c92f23f49ceebe97f4b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-26 14:18:23 -0800

    string capacity functions

--- Introduction to C++ for DSA/Module 5/capacity.cpp
+++ Introduction to C++ for DSA/Module 5/capacity.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
+    // string s = "Hello world";
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // string p = "dshhdhdhhhhskkhdiuuwydkshhksdhkdh";
+    // cout << p.capacity() << endl;
+    // string s = "Mukta";
+    // cout << s << endl;
+    // s.clear();
+    // cout << s << endl;
+    // cout << s.size() << endl;
+    // string s = "kkgk";
+    // if (s.empty() == true)
+    //     cout << "Empty" << endl;
+    // else
+    //     cout << "Not Empty" << endl;
+    string s;
+    cin >> s;
+    // s.resize(8);
+    // s.resize(10, 'm');
+    // cout << s.size() << endl;
+    // cout << s << endl;
+
+    s.resize(5);
+    cout << s << endl;
+    s.resize(11, 'x');
+    cout << s << endl;
 
     return 0;
 }
