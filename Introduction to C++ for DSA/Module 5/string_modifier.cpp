8469c3abb6cdd95aba78c5744666815e226cea23
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-26 17:35:05 -0800

    string Modifiers-ll

--- Introduction to C++ for DSA/Module 5/string_modifier.cpp
+++ Introduction to C++ for DSA/Module 5/string_modifier.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 int main()
 {
-    string a = "Mukta";
-    string b = "Thakur";
+    // string a = "Mukta";
+    // string b = "Thakur";
     // a += b;
     // a.append(b);
     // cout << a << endl;
     // cout << b << endl;
     // a = a + "A";
-    a.push_back('A');
-    a.pop_back();
-    a.pop_back();
+    // a.push_back('A');
+    // a.pop_back();
+    // a.pop_back();
+    // cout << a << endl;
+    // a = "Meghla";
+    // a.assign("Meghla");
+    // cout << a << endl;
+
+    string a = "HelloWorld";
+    // a.erase(4, 3);
+    // a.replace(4, 3, "so");
+    // a.replace(5, 0, "so");
+
+    a.insert(5, "Mukta");
     cout << a << endl;
 
     return 0;
