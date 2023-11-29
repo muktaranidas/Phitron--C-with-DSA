208c66d02987eb4a57055786d3cee5d09248f1bc
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-29 15:02:05 -0800

    O of sqrt of N time complexity

---
+++ Basic Data Structures/Module 1 - Time and Space Complexity/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+ // for (int i = 1; i <= sqrt(n); i++) // O(sqrt(N))
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << i << " ";
+    //         if (n / i != i)
+    //             cout << n / i << endl;
+    //     }
+    // }
