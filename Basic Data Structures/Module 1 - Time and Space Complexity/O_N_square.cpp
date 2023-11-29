d84e43fe3848bc48ec3610bababeae404721ab2a
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-29 15:34:02 -0800

    done

---
+++ Basic Data Structures/Module 1 - Time and Space Complexity/O_N_square.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    // for (int i = 1; i <= n; i++) // O(N*N)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << "Hello" << endl;
+    //     }
+    // }
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+        }
+    }
+    return 0;
+}
