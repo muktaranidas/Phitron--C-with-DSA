e6b37979c1e5cb97e57fa3af1c312dd2539657da
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-01 14:52:06 -0800

    element access

---
+++ Basic Data Structures/Module 2 - STL Vector/vector_access_and_iterator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+    // cout << v[v.size() - 1];
+    // cout << v.back(); //easy
+    // cout << v[0]; //easy
+    // cout << v.front();
+    return 0;
+}
